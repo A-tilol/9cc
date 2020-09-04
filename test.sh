88b41eb087937a9ddc35db23858e8eeb944f2b28
atilol <ggrkssk@gmail.com>
2020-09-04 10:37:53 +0900

    can skip spaces

--- test.sh
+++ test.sh
@@ -18,11 +18,12 @@ assert() {
 }
 
 # exit code
-assert 0 0
-assert 42 42
-
-# add, sub
-assert 21 '5+20-4'
+#assert 0 0
+#assert 42 42
+#
+## add, sub
+#assert 21 '5+20-4'
+assert 41 ' 12 + 34 - 5'
 
 echo OK
 
