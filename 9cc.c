87d2cabe6f56c2be22e876e7fc77a0d053132285
atilol <ggrkssk@gmail.com>
2020-09-04 12:51:19 +0900

    improve error message

--- 9cc.c
+++ 9cc.c
@@ -23,12 +23,18 @@ struct Token
     char *str;
 };
 
+char *user_input;
 Token *token;
 
-void error(char *fmt, ...)
+void error_at(char *loc, char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -45,14 +51,14 @@ bool consume(char op)
 void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c'", op);
+        error_at(token->str, "'%c'", op);
     token = token->next;
 }
 
 int expect_number()
 {
     if (token->kind != TK_NUM)
-        error("Not number.");
+        error_at(token->str, "Not number.");
     int val = token->val;
     token = token->next;
     return val;
@@ -99,7 +105,7 @@ Token *tokenize(char *p)
             continue;
         }
 
-        error("Couldn't tokenize.");
+        error_at(p, "Couldn't tokenize.");
     }
 
     new_token(TK_EOF, cur, p);
@@ -115,7 +121,8 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    token = tokenize(argv[1]);
+    user_input = argv[1];
+    token = tokenize(user_input);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
